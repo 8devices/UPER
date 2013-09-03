def3e45c80d78bb30406e9c575a00c26715f53cd
Giedrius <giedrius@8devices.com>
2013-09-03 17:41:50 +0300

    PWM0 corrections
    Fixed PWM0 clock frequency deviation due to incorrect divider value.
    Fixed PWM0 cycle and high time deviation.

--- src/Modules/LPC_PWM.c
+++ src/Modules/LPC_PWM.c
@@ -38,19 +38,19 @@ void lpc_pwm0_begin(SFPFunction *msg) {
 
 	if (SFPFunction_getArgumentType(msg, 0) != SFP_ARG_INT) return;
 
-	uint32_t p_cyclePeriod = SFPFunction_getArgument_int32(msg, 0) & 0xFFFF;	// 16 bit value in microseconds
+	uint32_t p_cyclePeriod = (SFPFunction_getArgument_int32(msg, 0)-1) & 0xFFFF;	// 16 bit value in microseconds
 
 	LPC_SYSCON->SYSAHBCLKCTRL |= BIT7;	// enable clock for CT16B0
 
 	// MAT3 is configured as the driving clock for PWM
 	LPC_CT16B0->TCR = BIT0 | BIT1;	// Enable timer, but keep in reset state
-	LPC_CT16B0->PR = 48;	// 48MHz/48 = 1MHz (1us)
+	LPC_CT16B0->PR = 48-1;	// 48MHz/48 = 1MHz (1us)
 
 	LPC_CT16B0->MCR = BIT10; // Reset timer on MR3
 	LPC_CT16B0->MR3 = p_cyclePeriod; // Set PWM cycle period
-	LPC_CT16B0->MR2 = p_cyclePeriod; // Set full low time for PWM2
-	LPC_CT16B0->MR1 = p_cyclePeriod; // Set full low time for PWM1
-	LPC_CT16B0->MR0 = p_cyclePeriod; // Set full low time for PWM0
+	LPC_CT16B0->MR2 = 0; // Set no low time for PWM2
+	LPC_CT16B0->MR1 = 0; // Set no low time for PWM1
+	LPC_CT16B0->MR0 = 0; // Set no low time for PWM0
 
 	LPC_CT16B0->EMR = 0x02A0; // External outputs enabled: channels 0-2 set high on match
 	LPC_CT16B0->PWMC = 0x7;   // PWM channels enabled: set channels 0-2 to PWM control
@@ -65,11 +65,14 @@ void lpc_pwm0_set(SFPFunction *msg) {
 	if (SFPFunction_getArgumentType(msg, 0) != SFP_ARG_INT || SFPFunction_getArgumentType(msg, 1) != SFP_ARG_INT) return;
 
 	uint32_t p_channelID = SFPFunction_getArgument_int32(msg, 0);			// PWM channel ID [0-2]
-	uint32_t p_highTime = SFPFunction_getArgument_int32(msg, 1) & 0xFFFF;	// PWM signal high time in microseconds
+	uint32_t p_highTime = SFPFunction_getArgument_int32(msg, 1);	// PWM signal high time in microseconds
 
 	if (p_channelID > 2) return;
 
-	LPC_CT16B0->MR[p_channelID] = (LPC_CT16B0->MR3 & 0xFFFF) - p_highTime;	// Set PWM low time
+	if (LPC_CT16B0->MR3 < p_highTime)
+		LPC_CT16B0->MR[p_channelID] = 0;	// Set full high time (0 low time)
+	else
+		LPC_CT16B0->MR[p_channelID] = LPC_CT16B0->MR3 + 1 - p_highTime;	// Set PWM low time
 }
 
 void lpc_pwm0_end(SFPFunction *msg) {
